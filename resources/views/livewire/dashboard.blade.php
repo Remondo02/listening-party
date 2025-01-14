131fb7f5a65205f2d41bbaa083c9f837c7f334a8
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2025-01-14 17:47:08 +0900

    Models and Migrations

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,9 +1,23 @@
 <?php
 
 use Livewire\Volt\Component;
+use App\Models\ListeningParty;
 
 new class extends Component {
-    //
+
+    public string $name = '';
+    public $startTime;
+
+    public function createListeningParty()
+    {
+    }
+
+    public function with()
+    {
+        return [
+            'listening_parties' => ListeningParty::all(),
+        ];
+    }
 }; ?>
 
 <div>
