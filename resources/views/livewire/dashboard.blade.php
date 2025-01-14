4a4a56eaf69133838fc317060a5ba43b63316318
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2025-01-14 18:07:13 +0900

    Adding WireUI component Library

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -4,7 +4,6 @@ use Livewire\Volt\Component;
 use App\Models\ListeningParty;
 
 new class extends Component {
-
     public string $name = '';
     public $startTime;
 
@@ -20,6 +19,12 @@ new class extends Component {
     }
 }; ?>
 
-<div>
-    Hello world.
+<div class="flex items-center justify-center min-h-screen bg-slate-50">
+    <div class="max-w-lg w-full px-4">
+        <form wire:submit="createListeningParty" class="space-y-6">
+            <x-input wire:model="name" placeholder="Listening Party Name" />
+            <x-datetime-picker wire:model="startTime" placeholder="Listening PartyStart Time" />
+            <x-button primary>Create Listening Party</x-button>
+        </form>
+    </div>
 </div>
