9d49b089910292520cb7c1d1f324d61073edd939
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2025-01-15 16:34:46 +0900

    Styling the Landing Page - Part 2

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -62,19 +62,37 @@ new class extends Component {
     </div>
     {{-- Bottom half: existing listening parties --}}
     <div class="my-20">
-        @if ($listeningParties->isEmpty())
-            <div>No awwdio listening parties started yet... 😔</div>
-        @else
-            @foreach ($listeningParties as $listeningParty)
-                <div wire:key="{{ $listeningParty->id }}">
-                    <x-avatar src="{{ $listeningParty->episode->podcast->artwork_url }}" size="xl"
-                        rounded="full" />
-                    <p> {{ $listeningParty->name }}</p>
-                    <p> {{ $listeningParty->episode->title }}</p>
-                    <p> {{ $listeningParty->episode->podcast->title }}</p>
-                    <p> {{ $listeningParty->start_time }}</p>
-                </div>
-            @endforeach
-        @endif
+        <div class="max-w-lg mx-auto">
+            <h3 class="mb-8 font-serif font-bold">Ongoing Listening Parties</h3>
+            <div class="bg-white rounded-lg shadow-lg">
+                @if ($listeningParties->isEmpty())
+                    <div>No awwdio listening parties started yet... 😔</div>
+                @else
+                    @foreach ($listeningParties as $listeningParty)
+                        <div wire:key="{{ $listeningParty->id }}">
+                            <a href="{{ route('parties.show', $listeningParty) }}" class="block">
+                                <div
+                                    class="flex items-center justify-between p-4 transition-all border-b border-gray-200 hover:bg-gray-50 duration-150 ease-in-out">
+                                    <div class="flex items-center space-x-4 ">
+                                        <div class=" flex-shrink-0">
+                                            <x-avatar src="{{ $listeningParty->episode->podcast->artwork_url }}"
+                                                size="xl" rounded="sm" alt="Podcast Artwork" />
+                                        </div>
+                                        <div class="flex-1 min-w-0">
+                                            <p class="text-[0.9rem] font-semibold truncate text-slate-900">{{ $listeningParty->name }}</p>
+                                            <div class="mt-0.8">
+                                                <p class="text-sm text-slate-600 truncate">{{ $listeningParty->episode->title }}</p>
+                                                <p class="text-slate-400 uppercase tracking-tighter text-[0.7rem]">{{ $listeningParty->episode->podcast->title }}</p>
+                                            </div>
+                                            <p class="mt-1 text-xs">{{ $listeningParty->start_time }}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+                @endif
+            </div>
+        </div>
     </div>
 </div>
