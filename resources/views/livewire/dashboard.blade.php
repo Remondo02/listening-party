ae1cea3661890c81bbe7ff128bdfa8c16c4436eb
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2025-01-15 14:49:29 +0900

    Styling the Landing Page - Part 1

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -22,7 +22,7 @@ new class extends Component {
 
         $episode = Episode::create([
             'media_url' => $this->mediaUrl,
-    ]);
+        ]);
 
         $listeningParty = ListeningParty::create([
             'episode_id' => $episode->id,
@@ -38,19 +38,43 @@ new class extends Component {
     public function with()
     {
         return [
-            'listening_parties' => ListeningParty::all(),
+            'listeningParties' => ListeningParty::where('is_active', true)->orderBy('start_time', 'ASC')->with('episode.podcast')->get(),
         ];
     }
 }; ?>
 
-<div class="flex items-center justify-center min-h-screen bg-slate-50">
-    <div class="max-w-lg w-full px-4">
-        <form wire:submit="createListeningParty" class="space-y-6">
-            <x-input wire:model="name" placeholder="Listening Party Name" />
-            <x-input wire:model="mediaUrl" placeholder="Podcast RSS Feed URL"
-                description="Entering the RSS feed will grab the latest episode" />
-            <x-datetime-picker wire:model="startTime" placeholder="Listening PartyStart Time" :min="now()->subDays(1)" />
-            <x-button type="submit">Create Listening Party</x-button>
-        </form>
+<div class="min-h-screen bg-emerald-50 flex flex-col pt-8">
+    {{-- Top half: create new listening party form  --}}
+    <div class="flex items-center justify-center p-4">
+        <div class="w-full max-w-lg">
+            <x-card shadow="lg" rounded="lg">
+                <h2 class="text-xl font-bold font-serif text-center">Let's listen together</h2>
+                <form wire:submit="createListeningParty" class="space-y-6 mt-6">
+                    <x-input wire:model="name" placeholder="Listening Party Name" />
+                    <x-input wire:model="mediaUrl" placeholder="Podcast RSS Feed URL"
+                        description="Entering the RSS feed will grab the latest episode" />
+                    <x-datetime-picker wire:model="startTime" placeholder="Listening PartyStart Time"
+                        :min="now()->subDays(1)" />
+                    <x-button type="submit" class="w-full">Create Listening Party</x-button>
+                </form>
+            </x-card>
+        </div>
+    </div>
+    {{-- Bottom half: existing listening parties --}}
+    <div class="my-20">
+        @if ($listeningParties->isEmpty())
+            <div>No awwdio listening parties started yet... 😔</div>
+        @else
+            @foreach ($listeningParties as $listeningParty)
+                <div wire:key="{{ $listeningParty->id }}">
+                    <x-avatar src="{{ $listeningParty->episode->podcast->artwork_url }}" size="xl"
+                        rounded="full" />
+                    <p> {{ $listeningParty->name }}</p>
+                    <p> {{ $listeningParty->episode->title }}</p>
+                    <p> {{ $listeningParty->episode->podcast->title }}</p>
+                    <p> {{ $listeningParty->start_time }}</p>
+                </div>
+            @endforeach
+        @endif
     </div>
 </div>
